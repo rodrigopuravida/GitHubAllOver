be173c84652758f9914310b9c8f43bbe06e31da8
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-19 13:58:45 -0800

    Adding code to call local server in network controller

--- NetworkController.swift
+++ NetworkController.swift
@@ -15,6 +15,19 @@ class NetworkController {
   init() {
     let ephemeralConfig = NSURLSessionConfiguration.ephemeralSessionConfiguration()
     self.urlSession = NSURLSession(configuration: ephemeralConfig)
+  }
+  
+  func fetchRepositoriesBasedOnSearch(searchString : String, callback : ([AnyObject]?, String) -> (Void)) {
+    let url = NSURL(string: "http://127.0.0.1.3000")
+    
+    let dataTask = self.urlSession.dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+      if error == nil {
+        println("I am inside fetchRepositoriesBasedOnSearch")
+      }
+    })
+    dataTask.resume()
+    
+    
     
   }
 

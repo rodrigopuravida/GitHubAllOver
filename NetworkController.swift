fceba0c66e7b543cd259861af12320f638ddcc60
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-19 13:23:01 -0800

    Adding network controller class

---
+++ NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  GitHubAllOver
+//
+//  Created by Rodrigo Carballo on 1/19/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+import Foundation

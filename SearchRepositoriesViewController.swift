1492db0082d33ec636244fe4368f65f3eca152de
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-19 14:42:08 -0800

    Implemented Search bar delegate

--- SearchRepositoriesViewController.swift
+++ SearchRepositoriesViewController.swift
@@ -8,28 +8,48 @@
 
 import UIKit
 
-class SearchRepositoriesViewController: UIViewController {
+class SearchRepositoriesViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
+  
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var searchBar: UISearchBar!
+  let networkController = NetworkController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      self.tableView.dataSource = self
+      self.searchBar.delegate = self
+      self.networkController.fetchRepositoriesBasedOnSearch("Swift", callback: { (items, errorDescription) -> (Void) in
+        
+      })
+  }
+  
+      func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+      }
+      
+      func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("REPO_CELL", forIndexPath: indexPath) as UITableViewCell
+        return cell
+      }
 
-        // Do any additional setup after loading the view.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+      
+//      func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("REPO_CELL", forIndexPath: indexPath) as UITableViewCell
+//        return cell
+//      }
+      //MARK: UISearchBarDelegate
+      
+      func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        println(searchBar.text)
+        searchBar.resignFirstResponder()
+        //make your network call here based on the search term
+      }
 }
+
+
+
+
+
